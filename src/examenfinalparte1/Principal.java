7ef62606577a7d99d76a47713f23e96907be3f5c
oracle <oracle@atanasoff08.danielcastelao.org>
2016-06-06 08:57:11 +0200

    Commit inicial - format

--- src/examenfinalparte1/Principal.java
+++ src/examenfinalparte1/Principal.java
@@ -1,9 +1,32 @@
 package examenfinalparte1;
+
 public class Principal {
+    
+//Variables
+    
     int i;
-public Principal(int j, int m) {i=met(j,m);}
+    
+//Constructores
+    
+    /*Sobrecarga de constructores*/
+    
+    public Principal(int j, int m) {
+        i = met(j, m);
+    }
+
     public Principal(int j) {
-        i=met(j,j);}
-    public Principal() { i=-1;
-    }public int met(int x, int x1) {
-      return x + x1;}}
+
+        i = met(j, j);
+    }
+
+    public Principal() {
+        i = -1;
+
+    }
+    
+//Métodos
+    
+    public int met(int x, int x1) {
+        return x + x1;
+    }
+}
