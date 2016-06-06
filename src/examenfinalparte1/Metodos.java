0ab9f03dd1a9beb01294e87cee6488cb15c840e2
oracle <oracle@atanasoff08.danielcastelao.org>
2016-06-06 09:16:05 +0200

    Commit Final - JavaDoc generado y todo explicado

--- src/examenfinalparte1/Metodos.java
+++ src/examenfinalparte1/Metodos.java
@@ -4,27 +4,51 @@ public class Metodos {
     
 //Variables
     
+    /**
+     *  Variable para igualar la suma
+     */
+    
     int VariableSuma;
     
 //Constructores
     
     /*Sobrecarga de constructores*/
     
+    /**
+     * Constructor que recibe dos valores diferentes y los suma y lo iguala a la variable establecida
+     * 
+     * @param primerValor
+     * @param segundoValor 
+     */
+    
     public Metodos(int primerValor, int segundoValor) {
         VariableSuma = Suma(primerValor, segundoValor);
     }
-
+    /**
+     * Constructor que recibe un valor único y lo suma por si mismo y lo iguala a la variable establecida
+     * 
+     * @param valorUnico 
+     */
     public Metodos(int valorUnico) {
 
         VariableSuma = Suma(valorUnico, valorUnico);
     }
-
+    /**
+     * Constructor que iguala la variable declarada a un valor
+     */
     public Metodos() {
         VariableSuma = -1;
 
     }
     
 //Métodos
+    
+    /**
+     * Metodo que recibe dos números, los suma y los 'retorna'
+     * @param numero1
+     * @param numero2
+     * @return 
+     */
     
     public int Suma(int numero1, int numero2) {
         return numero1 + numero2;
